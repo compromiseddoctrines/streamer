b5489f055c0d5e9390e7b38c02c15cf97ec6d38c
jonmateo <compromiseddoctrines@gmail.com>
2020-11-06 22:14:43 +0800

    add router package to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+//importing the router package
+
+const PageOne = () => {
+    return (
+        <div> Page One
+            {/* Link Tag Equal to a href in html */}
+            <Link to="/pageTwo">
+                Navigate to Page Two
+            </Link>
+        </div>
+    );
+}
+
+const PageTwo = () => {
+    return (
+        <div> Page Two
+            <Link to="/">
+                Go to Page One
+            </Link>
+        </div>
+        
+    );
+}
 
 const App = () => {
     return(
         <div>
-            App
+            <BrowserRouter>
+                <div>
+                    {/* exact path of the given value */}
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pageTwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
