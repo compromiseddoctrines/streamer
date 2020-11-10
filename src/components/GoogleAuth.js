3d270dfea2441adf0202b07b2e2a833106718096
jonmateo <compromiseddoctrines@gmail.com>
2020-11-10 22:10:04 +0800

    added google auth buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 class GoogleAuth extends React.Component{
 
+    state = { isSignedIn: null }
+
     componentDidMount(){
 
         // initialize google Oauth via gapi
@@ -9,13 +11,59 @@ class GoogleAuth extends React.Component{
             window.gapi.client.init({
                 clientId: '42905327854-2hfv96pfthfom3ipdf3ot5sp3o4kotpu.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
+                //returns TRUE or FALSE boolean if signed IN
             });
         });
+    };
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    };
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn){
+            return(
+                <div>
+                    <button 
+                        className="ui red google button"
+                        onClick={this.onSignOutClick}
+                    >
+                        <i className="google icon" />
+                        Sign Out
+                    </button>
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                    <button 
+                        className="ui blue google button"
+                        onClick={this.onSignInClick}>
+                        <i className="google icon" />
+                        Sign In with Google
+                    </button>
+                </div>
+            );
+        }
     }
 
     render(){
         return(
-            <div> Google Auth</div>
+            <div>{this.renderAuthButton()}</div>
         );
     }
 }
